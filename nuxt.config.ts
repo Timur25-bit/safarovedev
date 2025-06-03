b540a32fd6848ceb1f6db41bb7ddc844909ba287
timur.safarov <safarov01051996@gmail.com>
2025-06-03 21:42:34 +0300

    Настройка деплоя на gh pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
+  ssr: true,
   devtools: { enabled: true },
+  nitro: {
+    preset: 'static',
+    static: {
+      fallback: '200.html'
+    }
+  },
+  app: {
+    baseURL: '/safarovedev/'
+  },
   modules: [
     '@nuxt/eslint',
     '@nuxt/fonts',
